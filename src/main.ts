e987552a2552f4e5e435d631bc86f5031f09f206
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-06-15 13:23:36 +1000

    add code, dependencies, build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const pattern = core.getInput('pattern')
+    const regex = new RegExp(pattern)
+    const title =
+      github.context.payload &&
+      github.context.payload.pull_request &&
+      github.context.payload.pull_request.title
+    core.info(title)
+    const isValid = regex.test(title)
+    if (!isValid) {
+      core.setFailed(
+        `Pull request title "${title}" does not match regex pattern "${pattern}".`
+      )
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
